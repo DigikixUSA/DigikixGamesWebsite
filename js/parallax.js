b347c2cf8dfe84b3cc95237a850263dee3e25fed
ChrisM <cmick19@wgu.edu>
2025-07-23 11:07:29 -0500

    Initial Commit

---
+++ js/parallax.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const bg = document.getElementById('parallax-bg');
+
+    window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY;
+        bg.style.transform = `translateY(${scrollY * 0.5}px)`;
+    });
+});
